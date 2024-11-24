acd4b21266280454787fa37bd82bda3bb26c3abb
$Ü₹PLUŠ🤑 <jamesstorms478@gmail.com>
2024-11-24 18:15:30 +0100

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://grabngo-xtrasupermarket.vercel.app/',
+        target: 'https://grabngo-xtrasupermarket.vercel.app/api/json-server',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
